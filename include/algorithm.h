07328a37939cc1de0e80bb5e3c109425805250a3
Wenjie Wang <ww107587@gmail.com>
2024-06-23 15:33:28 +0800

    缩进代码

--- include/algorithm.h
+++ include/algorithm.h
@@ -10,4 +10,5 @@ typedef unsigned short rgb565le;
 typedef unsigned char rgb888[3];
 void yuyv2rgb(unsigned char *yuyv, unsigned char *rgb, int width, int height);
 void rgb565le2rgb888(rgb565le *prgb565le, rgb888 *prgb888, unsigned int width, unsigned int height);
+void yuyv2h264(unsigned char *yuyv, unsigned char *h264, unsigned int width, unsigned int height);
 #endif
